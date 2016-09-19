c0670db96ea9b2e6f1bbadc67e9abf38c5a46272
Conner Simmering <connersimmering@gmail.com>
2016-09-19 15:12:38 -0600

    gui-test

---
+++ MyDrawPanel.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Conner on 9/19/16.
+ */
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MyDrawPanel {
+
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.orange);
+
+        g.fillRect(20,50,100,100);
+    }
+
+}
---
+++ SimpleGui1.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Conner on 9/19/16.
+ */
+
+import javax.swing.*;
+
+public class SimpleGui1 {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+
+        JButton button = new JButton("click me");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(button);
+
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+}
